02233063705748f6633a86622f7a4455c43cec9b
TheSongOfSongs <banecs2379@gmail.com>
2022-03-06 10:02:54 +0900

    Feat: ViewModelType 추가

---
+++ DevEvent/ViewModel/ViewModelType.swift
@@ -0,0 +1,13 @@
+//
+//  ViewModelType.swift
+//  DevEvent
+//
+//  Created by Jinhyang Kim on 2022/03/05.
+//
+
+import Foundation
+
+protocol ViewModelType: AnyObject {
+    associatedtype Input
+    associatedtype Output
+}
