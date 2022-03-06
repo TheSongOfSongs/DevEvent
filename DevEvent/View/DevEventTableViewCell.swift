7b8052addab06540c6958ff357ffd533714f3100
TheSongOfSongs <banecs2379@gmail.com>
2022-03-06 20:31:14 +0900

    Feat: DevEventTableCell 추가

---
+++ DevEvent/View/DevEventTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  DevEventTableViewCell.swift
+//  DevEvent
+//
+//  Created by Jinhyang Kim on 2022/03/06.
+//
+
+import UIKit
+
+class DevEventTableViewCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var favoriteImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func prepareForReuse() {
+        favoriteImageView.isHidden = true
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
