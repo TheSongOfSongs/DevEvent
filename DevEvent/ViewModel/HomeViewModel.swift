808a9238eaf081d8352c0e74fcb7f51d00c61deb
TheSongOfSongs <banecs2379@gmail.com>
2022-03-06 20:30:59 +0900

    Feat: HomeViewModel 파일 추가

---
+++ DevEvent/ViewModel/HomeViewModel.swift
@@ -0,0 +1,28 @@
+//
+//  HomeViewModel.swift
+//  DevEvent
+//
+//  Created by Jinhyang Kim on 2022/03/06.
+//
+
+import Foundation
+
+class HomeViewModel: ViewModelType {
+    
+    var input = Input()
+    var output = Output()
+    
+    struct Input {
+        
+    }
+    
+    struct Output {
+        
+    }
+    
+
+    // MARK: - init
+    init() {
+        
+    }
+}
