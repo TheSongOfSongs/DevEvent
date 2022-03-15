a1e40eec31a274e7660996290461eeb4de93813b
TheSongOfSongs <banecs2379@gmail.com>
2022-03-15 22:00:54 +0900

    Feat: Event 정보로 CoreData에서 객체 받아오는 메서드 추가

--- EventCoreData+CoreDataClass.swift
+++ EventCoreData+CoreDataClass.swift
@@ -13,3 +13,20 @@ import CoreData
 public class EventCoreData: NSManagedObject {
 
 }
+
+extension EventCoreData {
+    class func fetchRequest(event: Event) -> NSFetchRequest<EventCoreData> {
+        let request = NSFetchRequest<EventCoreData>(entityName: Entity.eventCoreData.name)
+        
+        let predicate: NSPredicate = {
+            if let eventURL = event.url {
+                return NSPredicate(format: "name == %@ && url = %@", event.name, eventURL as CVarArg)
+            } else {
+                return NSPredicate(format: "name == %@", event.name)
+            }
+        }()
+        
+        request.predicate = predicate
+        return request
+    }
+}
